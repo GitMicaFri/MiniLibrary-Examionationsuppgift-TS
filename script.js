8177a4a45a51c49f24d1513403b241aecb4d3d6e
GitMicaFri <micaela.fristedt@gmail.com>
2024-01-25 10:00:05 +0100

    colorchanges

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let bookList;
 
-async function getBookData() {
-  // hämta data/API
+async function getBookData() { // Hämta data/API
   let response = await fetch(
     'https://my-json-server.typicode.com/zocom-christoffer-wallenberg/books-api/books'
   );
@@ -20,21 +19,18 @@ function updateBookElement(bookData, element) {
 }
 
 function updatePage1(bookList) {
-  // uppdate title author 1st page
   const bookElements = document.querySelectorAll('.book'); // Här hämtar vi listan med alla böcker för att få ut en lista med index
   bookList.forEach((bookData, index) => {
-    const bookElement = bookElements[index]; // refererar till samma index som i vår array
+    const bookElement = bookElements[index]; // Refererar till samma index som i vår array
     updateBookElement(bookData, bookElement);
   });
 }
 
-function updatePage2(selectedBookData, newId) {
-  // uppdate title author 2nd page. funktionen förväntar sig att få den valda boken som input, samt vilket nytt Id elementet på sida 2 ska ha.
-
+// Uppdatera bokomslaget.
+function updatePage2(selectedBookData, newId) {  // Funktionen förväntar sig att få den valda boken som input, samt vilket nytt Id elementet på sida 2 ska ha.
   const bookCoverElement = document.querySelector('.book-cover');
   const bookDescriptionElement = document.querySelector('.book-description');
   bookCoverElement.id = newId;
-  // uppdatera bokomslaget
   const titleCoverElement = bookCoverElement.querySelector('.title');
   const authorCoverElement = bookCoverElement.querySelector('.author');
 
@@ -44,30 +40,19 @@ function updatePage2(selectedBookData, newId) {
   // uppdatera bokbeskrivningen
   const titleDescElement = bookDescriptionElement.querySelector('.title');
   const authorDescElement = bookDescriptionElement.querySelector('.author');
-  const descriptionElement =
-    bookDescriptionElement.querySelector('.description');
+  const descriptionElement = bookDescriptionElement.querySelector('.description');
   const factsElement = bookDescriptionElement.querySelector('.facts');
 
   titleDescElement.textContent = selectedBookData.title;
   authorDescElement.textContent = selectedBookData.author;
   descriptionElement.textContent = selectedBookData.description;
-  factsElement.querySelector(
-    'h4:nth-child(1)'
-  ).textContent = `Audience: ${selectedBookData.audience}`;
-  factsElement.querySelector(
-    'h4:nth-child(2)'
-  ).textContent = `First published: ${selectedBookData.year}`;
-  factsElement.querySelector(
-    'h4:nth-child(3)'
-  ).textContent = `Pages: ${selectedBookData.pages}`;
-  factsElement.querySelector(
-    'h4:nth-child(4)'
-  ).textContent = `Publisher: ${selectedBookData.publisher}`;
+  factsElement.querySelector('h4:nth-child(1)').textContent = `Audience: ${selectedBookData.audience}`;
+  factsElement.querySelector('h4:nth-child(2)').textContent = `First published: ${selectedBookData.year}`;
+  factsElement.querySelector('h4:nth-child(3)').textContent = `Pages: ${selectedBookData.pages}`;
+  factsElement.querySelector('h4:nth-child(4)').textContent = `Publisher: ${selectedBookData.publisher}`;
 }
 
 function setUpBackButton() {
-  // event listener for back button
-  //back-button
   const backButton = document.querySelector('#back-btn');
   if (backButton) {
     backButton.addEventListener('click', function () {
@@ -78,7 +63,6 @@ function setUpBackButton() {
 }
 
 function setUpReadButton() {
-  // event listener for read button
   const readButton = document.querySelector('#read-btn');
   if (readButton) {
     readButton.addEventListener('click', function () {});
@@ -86,9 +70,8 @@ function setUpReadButton() {
 }
 
 function setUpBookClicks(bookList) {
-  // for every book in bookList, add event listender thet updates page 2 with info about klicked book
   const books = document.querySelectorAll('.book');
-  books.forEach((book, index) => {
+  books.forEach((book, index) => {  //För varje klickad bok ska sidan 2 uppdateras med rätt bok enligt index
     book.addEventListener('click', function () {
       const selectedBookData = bookList[index];
       updatePage2(selectedBookData, this.id);
@@ -98,7 +81,7 @@ function setUpBookClicks(bookList) {
   });
 }
 
-window.onload = function () {
+window.onload = function () { //För att all data ska hinna laddas
   bookList = getBookData().then((bookList) => {
     updatePage1(bookList);
     setUpBookClicks(bookList);
